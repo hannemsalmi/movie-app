c1172ba11df9f01f2e570dfe73fa7680365282b9
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-07-07 17:33:40 +0300

    added Movie page, added .env to gitignore

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,7 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
 const Movies = () => {
+
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+
+    const fetchMovies = async () => {
+
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+          );
+
+        setContent(data.results);
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    }, [page])
+
     return (
         <div>
             <span className='pageTitle'>Movies</span>
+            <div className="movies">
+            {
+                content && content.map((c) => (
+                    <SingleContent key={c.id} id={c.id} poster={c.poster_path} title={c.title || c.name} date={c.first_air_date || c.release_date} media_type={c.media_type} vote_average={c.vote_average}/>
+                ))
+            }
+            </div>
+            <CustomPagination setPage={setPage}/>
         </div>
     )
 }
