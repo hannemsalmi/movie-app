c77b8bffc0ab5541062a6173461248e103deefc5
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-08-24 20:14:02 +0300

    Fetched series, added Movies and Series css files

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,9 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import './Series.css';
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
 const Series = () => {
-    return (
-        <div>
-            <span className='pageTitle'>Series</span>
-        </div>
-    )
-}
+        const [page, setPage] = useState(1);
+        const [content, setContent] = useState([]);
+    
+        const fetchSeries = async () => {
+    
+            const { data } = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+              );
+    
+            setContent(data.results);
+        };
+    
+        useEffect(() => {
+            fetchSeries();
+        }, [page])
+    
+        return (
+            <div>
+                <span className='pageTitle'>Series</span>
+                <div className="series">
+                {
+                    content && content.map((c) => (
+                        <SingleContent key={c.id} id={c.id} poster={c.poster_path} title={c.title || c.name} date={c.first_air_date || c.release_date} media_type={c.media_type} vote_average={c.vote_average}/>
+                    ))
+                }
+                </div>
+                <CustomPagination setPage={setPage}/>
+            </div>
+        )
+    }
 
 export default Series
