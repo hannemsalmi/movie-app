9ae6ebcdce36ed3de276b6b09c55fa61e3607732
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-09-01 18:14:31 +0300

    Added genre filter for movies

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -3,28 +3,65 @@ import { useEffect, useState } from "react";
 import SingleContent from "../../components/SingleContent/SingleContent";
 import './Movies.css';
 import CustomPagination from "../../components/Pagination/CustomPagination";
+import Genres from "../../components/Genres/Genres";
 
-const Movies = () => {
+    const Movies = () => {
+        const [page, setPage] = useState(1);
+        const [content, setContent] = useState([]);
+        const [genres, setGenres] = useState([]);
+        const [selectedGenres, setSelectedGenres] = useState(['all']);
+        const media_type = 'movie';
 
-    const [page, setPage] = useState(1);
-    const [content, setContent] = useState([]);
-    const media_type = 'movie';
+        const fetchMovies = async () => {
+            let apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`;
+        
+            if (selectedGenres.length > 0 && !selectedGenres.includes('all')) {
+                const selectedGenresString = selectedGenres.join(',');
+                apiUrl += `&with_genres=${selectedGenresString}`;
+            }
+        
+            const { data } = await axios.get(apiUrl);
+            setContent(data.results);
+        };
 
-    const fetchMovies = async () => {
-
-        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
-          );
-
-        setContent(data.results);
+    const fetchGenres = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+        setGenres(data.genres);
     };
 
     useEffect(() => {
+        fetchGenres();
         fetchMovies();
-    }, [page])
+    }, [page, selectedGenres]);
+
+    
+    const handleGenreClick = (genreId) => {
+        setSelectedGenres([genreId]);
+        setPage(1);
+    };
 
     return (
         <div>
             <span className='pageTitle'>Movies</span>
+            <div className="genres">
+                    <Genres
+                        key="all"
+                        id="all"
+                        name="All"
+                        onGenreClick={handleGenreClick}
+                        selected={selectedGenres.includes('all')}
+                    />
+
+                    {genres && genres.map((g) => (
+                        <Genres
+                            key={g.id}
+                            id={g.id}
+                            name={g.name}
+                            onGenreClick={handleGenreClick}
+                            selected={selectedGenres.includes(g.id)}
+                        />
+                    ))}
+                </div>
             <div className="movies">
             {
                 content && content.map((c) => (
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -3,15 +3,25 @@ import { img_300, unavailable } from "../../config/config";
 import './SingleContent.css';
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
+
+    const releaseDate = new Date(date);
+
+    const formattedDate = releaseDate.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+
+
     return (
         <div className="media">
             <Badge badgeContent={vote_average} color={vote_average > 7 ? "primary" : "secondary"} />
             <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
             <b className="title">{title}</b>
             <span className="subTitle">
-                {media_type === "movie" ? "Movie" : "TV Series"}
+                {media_type === "tv" ? "TV Series" : "Movie"}
             </span>
-            <span className="subTitle">{date}</span>
+            <span className="subTitle">{formattedDate}</span>
         </div>
     )
 }
