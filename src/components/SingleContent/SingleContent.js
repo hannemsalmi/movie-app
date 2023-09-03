edd4169c04c363f67dab9353b5f8826b6e5d04a9
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-09-03 21:12:18 +0300

    Changed badge colors for SingleContent.js

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -15,7 +15,9 @@ const SingleContent = ({ id, poster, title, date, media_type, vote_average }) =>
 
     return (
         <div className="media">
-            <Badge badgeContent={vote_average} color={vote_average > 7 ? "primary" : "secondary"} />
+            <Badge badgeContent={vote_average}
+            color={vote_average > 8 ? "success" : vote_average > 5 ? "warning" : "error"}
+            />
             <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
             <b className="title">{title}</b>
             <span className="subTitle">
